51a2761f59a5496aa301761fc260825a11430ae0
Aryan Swaminath <aryan.swaminath@aol.com>
2022-10-17 22:36:10 -0500

    Added player movement vectors

--- main.cpp
+++ main.cpp
@@ -11,8 +11,6 @@ int main(){
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
     Vector2 mapPos = {0.0, 0.0};
 
-    // character speed
-
     //target fps
     SetTargetFPS(60);
 
@@ -20,7 +18,14 @@ int main(){
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE);
-        
+        Vector2 direction{};
+        if(IsKeyDown(KEY_A)) direction.x -= 1.0; 
+        if(IsKeyDown(KEY_D)) direction.x += 1.0;
+        if(IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if(IsKeyDown(KEY_S)) direction.y += 1.0;
+
+        if(Vector2Length(direction) != 0.0) mapPos = Vector2Subtract(mapPos, Vector2Normalize(direction));
+
         DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
         EndDrawing();
